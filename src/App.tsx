12314568f979a805393c15fcca1d35391def5eee
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-12-08 20:07:20 +0700

    Implement Button - Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button';
 import Greet from './components/Greet';
 import Heading from './components/Heading';
+import Input from './components/Input';
 import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
@@ -22,13 +24,24 @@ function App() {
   return (
     <div className="App">
       <Greet name="World" messageCount={20} isLoggedIn={true} />
+
       <Person name={personName} />
+
       <PersonList name={nameList} />
+
       <Status status='error' />
+
       <Heading>Children</Heading>
+
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicaprio</Heading>
       </Oscar>
+
+      <Button handleClick={(event, id) => {
+        console.log("Button clicked", event, id );
+      }} />
+
+      <Input value='' handleChange={event => console.log(event)} />
     </div>
   );
 }
